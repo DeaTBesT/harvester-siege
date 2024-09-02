bc3131bb7ee93c0fc19bbdc77629eef5968d6d6a
DeaTBesT <niliukhin@gmail.com>
2024-09-03 00:59:44 +0300

    feat: сереализатор для scriptable object

---
+++ Assets/Scripts/Utils/NetworkSerializer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class NetworkSerializer
+    {
+        public static string SerializeScriptableObject(ScriptableObject scriptableObject) =>
+            scriptableObject.name; 
+        
+        public static ScriptableObject DeserializeScriptableObject(string scriptableObject) =>
+            Resources.Load(scriptableObject) as ScriptableObject;
+    }
+}
