a1d6148680e53442822738ca0fc876dc817d437b
DeaTBesT <niliukhin@gmail.com>
2024-09-03 01:00:33 +0300

    feat: система инвентаря сущности

---
+++ Assets/Scripts/Core/EntityInventoryController.cs
@@ -0,0 +1,26 @@
+﻿using GameResources;
+using Interfaces;
+using Mirror;
+
+namespace Core
+{
+    public class EntityInventoryController : NetworkBehaviour, IInitialize
+    {
+        public bool IsEnable { get; set; }
+        
+        public virtual void Initialize(params object[] objects)
+        {
+            
+        }
+
+        public virtual void AddResource(ResourceData resourceData)
+        {
+            
+        }
+        
+        public virtual void RemoveResource(ResourceData resourceData)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInventoryController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Core;
+using GameResources;
+using Managers;
+
+namespace Player
+{
+    public class PlayerInventoryController : EntityInventoryController
+    {
+        private GameResourcesManager _gameResourcesManager;
+
+        private List<ResourceData> _resourcesData = new();
+        
+        public override void Initialize(params object[] objects) => 
+            _gameResourcesManager = objects[0] as GameResourcesManager;
+
+        public override void AddResource(ResourceData resourceData)
+        {
+            _gameResourcesManager.AddResource(resourceData.gameResourceDataConfig, resourceData.AmountResource);
+            _resourcesData.Add(resourceData);
+        }
+    }
+}
