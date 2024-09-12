050cec91e5af340de6632d3c8e99a0785ec5b308
DeaTBesT <niliukhin@gmail.com>
2024-09-12 13:58:10 +0300

    feat: добавил интерфейс загрузчика

---
+++ Assets/Scripts/Interfaces/INetworkLoad.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+
+namespace Interfaces
+{
+    public interface INetworkLoad
+    {
+        void LoadDataCmd(NetworkConnectionToClient conn);
+
+        void LoadDataServer(NetworkConnectionToClient conn);
+
+        void LoadDataRpc(NetworkConnectionToClient target, byte[] writerData);
+    }
+}
