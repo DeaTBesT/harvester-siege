0a042be0f1e16afc78905a38e7376c7752d91e37
DeaTBesT <niliukhin@gmail.com>
2024-09-16 00:51:53 +0300

    feat: вынес загрузку данных в отдельный класс

---
+++ Assets/Scripts/Managers/NetworkLoadManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Core;
+using Interfaces;
+using Mirror;
+using UnityEngine;
+
+namespace Managers
+{
+    public class NetworkLoadManager : Singleton<NetworkLoadManager>
+    {
+        private List<INetworkLoad> _networkLoaders = new();
+
+        public override void OnStartServer() =>
+            NetworkServer.OnConnectedEvent += OnPlayerConnected;
+
+        public override void OnStopServer() => 
+            NetworkServer.OnConnectedEvent -= OnPlayerConnected;
+
+        private void OnPlayerConnected(NetworkConnectionToClient conn) => 
+            StartCoroutine(LoadRoutine(conn));
+
+        private IEnumerator LoadRoutine(NetworkConnectionToClient conn)
+        {
+            while (conn.identity == null)
+            {
+                yield return null;
+            }
+
+            Debug.Log($"{conn.identity.name} is exist");
+            
+            if (!conn.identity.TryGetComponent(out EntityInitializer initializer))
+            {
+                Debug.LogError($"Entity initializer is null: {conn.connectionId}");
+            }
+
+            while (!initializer.IsInitialized)
+            {
+                yield return null;
+            }
+
+            Debug.Log($"{conn.identity.name} is initialized");
+            
+            if (!conn.identity.isLocalPlayer)
+            {
+                Debug.Log($"{conn.identity.name}, start load data");
+                LoadAllData(conn);
+            }
+        }
+
+        private void LoadAllData(NetworkConnectionToClient target)
+        {
+            foreach (var loader in _networkLoaders.Where(loader => loader != null))
+            {
+                loader.LoadDataServer(target);
+            }
+        }
+
+        public void AddLoader(INetworkLoad loader)
+        {
+            var isContains = _networkLoaders.Contains(loader);
+
+            if (isContains)
+            {
+                Debug.LogWarning("Loader is contains");
+                return;
+            }
+
+            _networkLoaders.Add(loader);
+        }
+
+        public void RemoveLoader(INetworkLoad loader) =>
+            _networkLoaders.Remove(loader);
+    }
+}
