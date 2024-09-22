ed11bd38c661cd35464e17b16e19c0469b74e0af
DeaTBesT <niliukhin@gmail.com>
2024-09-22 13:24:51 +0300

    feat: добавил поле для префаба

--- Assets/Scripts/GameResources/GameResourceData.cs
+++ Assets/Scripts/GameResources/GameResourceData.cs
@@ -9,12 +9,16 @@ namespace GameResources
         [SerializeField] private string _description = "empty";
 
         [SerializeField] private Sprite _resourceSprite;
-        
+
         [SerializeField] private GameResourceType _gameResourceType;
 
-        public string Title=> _title;
-        public string Description=> _description;
-        public Sprite ResourceSprite=> _resourceSprite;
-        public GameResourceType TypeGameResource=> _gameResourceType;
+        [Space]
+        [SerializeField] private GameObject _resourcePrefab;
+
+        public string Title => _title;
+        public string Description => _description;
+        public Sprite ResourceSprite => _resourceSprite;
+        public GameResourceType TypeGameResource => _gameResourceType;
+        public GameObject ResourcePrefab => _resourcePrefab;
     }
 }
