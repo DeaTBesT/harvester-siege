3fd40d58983068291162aabd4f5eca3a598004eb
DeaTBesT <niliukhin@gmail.com>
2024-09-29 01:14:03 +0300

    feat: создание контейнера объектов, само инициализация и деинициализация

--- Assets/Scripts/Core/EntityInitializer.cs
+++ Assets/Scripts/Core/EntityInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Mirror;
+﻿using System.Collections;
+using Mirror;
 using UnityEngine;
 
 namespace Core
@@ -6,11 +7,18 @@ namespace Core
     public abstract class EntityInitializer : NetworkBehaviour
     {
         [SerializeField] private bool _selfInitialize = true;
+        [SerializeField] private bool _selfDeinitialize = true;
+
+        [SerializeField] private GameObject _entityObjectContainerPrefab;
+        
+        protected Transform _entityObjectContainer;
         
         public bool IsInitialized { get; protected set; }
 
         public override void OnStartClient()
         {
+            CreateEntityObjectContainer();
+            
             if (!_selfInitialize)
             {
                 return;
@@ -21,7 +29,7 @@ namespace Core
 
         public override void OnStopClient()
         {
-            if (!_selfInitialize)
+            if (!_selfDeinitialize)
             {
                 return;
             }
@@ -33,6 +41,32 @@ namespace Core
 
         public virtual void Deinitialize()
         {
+            if (_entityObjectContainer != null)
+            {
+                NetworkServer.Destroy(_entityObjectContainer.gameObject);
+            }
         }
+
+        private void CreateEntityObjectContainer()
+        {
+            CreateEntityObjectContainerCmd();
+        }
+        
+        [Command]
+        private void CreateEntityObjectContainerCmd() => 
+            StartCoroutine(CreateEntityObjectContainerHandler());
+
+        private IEnumerator CreateEntityObjectContainerHandler()
+        {
+            while (isOwned)
+            {
+                yield return null;
+            }
+            
+            _entityObjectContainer = Instantiate(_entityObjectContainerPrefab, Vector3.zero, Quaternion.identity).transform;
+            _entityObjectContainer.name = gameObject.name + " object container";
+            
+            NetworkServer.Spawn(_entityObjectContainer.gameObject, connectionToClient);
+        } 
     }
 }
