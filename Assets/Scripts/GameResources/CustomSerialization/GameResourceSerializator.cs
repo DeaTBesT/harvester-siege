24c4396ac0e9c0b0d014b35302d4f75c195ea082
DeaTBesT <niliukhin@gmail.com>
2024-10-27 03:07:15 +0300

    feat: добавил кастомную сериализацию

---
+++ Assets/Scripts/GameResources/CustomSerialization/GameResourceSerializator.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+
+namespace GameResources.CustomSerialization
+{
+    public struct GameResourceData
+    {
+        public int Amount { get; }
+
+        public GameResourceData(int amount) => 
+            Amount = amount;
+    }
+    
+    public static class GameResourceSerializator
+    {
+        public static void WriteGameResourceData(this NetworkWriter writer,
+            GameResourceData data) =>
+            writer.WriteInt(data.Amount);
+
+        public static GameResourceData ReadGameResourceData(this NetworkReader reader) => 
+            new(reader.ReadInt());
+    }
+}
