e0de689fe10fc59d2c218a40e2771e14bbe0b7ac
DeaTBesT <niliukhin@gmail.com>
2024-10-28 01:54:43 +0300

    feat: сделал компонент спавна ресурсов

---
+++ Assets/Scripts/GameResources/Core/ResourceSpawner.cs
@@ -0,0 +1,34 @@
+﻿using Interfaces;
+using Mirror;
+using UnityEngine;
+
+namespace GameResources.Core
+{
+    public class ResourceSpawner : NetworkBehaviour, IInstantiateResource
+    {
+        [SerializeField] private ResourceData _resourceData;
+        
+        public void InstantiateResource(string resourceName = default, int amount = default)
+        {
+            if (isServer)
+            {
+                InstantiateResourceServer(resourceName, amount);
+            }
+            else
+            {
+                InstantiateResourceCmd(resourceName, amount);
+            }
+        }
+
+        [Command(requiresAuthority = false)]
+        public void InstantiateResourceCmd(string resourceName, int amount) => 
+            InstantiateResourceServer(resourceName, amount);
+
+        [Server]
+        public void InstantiateResourceServer(string resourceName, int amount) => 
+            InstantiateResourceInternal(resourceName, amount);
+
+        protected virtual void InstantiateResourceInternal(string resourceName, int amount) => 
+            ResourceData.InstantiateResource(_resourceData, transform.position);
+    }
+}
