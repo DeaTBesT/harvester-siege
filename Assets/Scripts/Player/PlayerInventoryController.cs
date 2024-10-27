0c36528fd45cf04ed110b6c68238ad2802f3d9c0
DeaTBesT <niliukhin@gmail.com>
2024-10-27 03:20:11 +0300

    feat and fix: добавил добавление деинициализацию, добавление предметов и сброс предметов, пофиксил баги

--- Assets/Scripts/Player/PlayerInventoryController.cs
+++ Assets/Scripts/Player/PlayerInventoryController.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Core;
-using GameResources;
+using GameResources.Core;
 using Managers;
+using Mirror;
+using UnityEngine;
+using Utils.Networking;
 
 namespace Player
 {
@@ -10,24 +14,113 @@ namespace Player
         private GameResourcesManager _gameResourcesManager;
 
         private List<ResourceData> _resourcesData = new();
-        
-        public override void Initialize(params object[] objects) => 
+
+        public override void Initialize(params object[] objects) =>
             _gameResourcesManager = objects[0] as GameResourcesManager;
 
-        public override void AddResource(ResourceData resourceData)
+        public override void Deinitialize() =>
+            DropAllResources();
+
+        public override void AddResource(ResourceData addResourceData) =>
+            AddResourceCmd(NetworkScriptableObjectSerializer.SerializeScriptableObject(addResourceData.ResourceConfig),
+                addResourceData.AmountResource);
+
+        [Command]
+        private void AddResourceCmd(string gameResourceName, int amount) =>
+            AddResourceRpc(gameResourceName, amount);
+
+        [ClientRpc]
+        private void AddResourceRpc(string gameResourceName, int amount)
         {
-            _gameResourcesManager.AddResource(resourceData.gameResourceDataConfig, resourceData.AmountResource);
-            _resourcesData.Add(resourceData);
+            var gameResource =
+                (ResourceConfig)NetworkScriptableObjectSerializer.DeserializeScriptableObject(gameResourceName);
+            var resourceData = _resourcesData.FirstOrDefault(x =>
+                x.ResourceConfig.TypeResource == gameResource.TypeResource);
+
+            if (resourceData != null)
+            {
+                resourceData.AddResource(amount);
+            }
+            else
+            {
+                _resourcesData.Add(new ResourceData(gameResource, amount));
+            }
+
+            _gameResourcesManager.AddResource(gameResource, amount);
         }
 
-        public override void DropResource(ResourceData resourceData) => 
-            ResourceData.InstantiateResource(resourceData, transform.position);
+        public override void DropResource(ResourceData resourceData)
+        {
+            var resourceName = NetworkScriptableObjectSerializer.SerializeScriptableObject(resourceData.ResourceConfig);
+            var resourceAmount = resourceData.AmountResource;
+
+            InstanceResource(resourceName, resourceAmount);
+
+            _gameResourcesManager.RemoveResource(resourceData.ResourceConfig, resourceAmount);
+            RemoveResourceDataCmd(resourceName);
+        }
+
+        private void InstanceResource(string resourceName, int amount)
+        {
+            if (isServer)
+            {
+                InstanceResourceServer(resourceName, amount);
+            }
+            else
+            {
+                InstanceResourceCmd(resourceName, amount);
+            }
+        }
+        
+        [Command]
+        private void InstanceResourceCmd(string resourceName, int amount) =>
+            InstanceResourceServer(resourceName, amount);
+
+        [Server]
+        private void InstanceResourceServer(string resourceName, int amount) =>
+            ResourceData.InstantiateResource(resourceName, amount, transform.position);
+
+        [Command]
+        private void RemoveResourceDataCmd(string resourceName) =>
+            RemoveResourceDataRpc(resourceName);
+
+        [ClientRpc]
+        private void RemoveResourceDataRpc(string resourceName)
+        {
+            var resourceConfig =
+                (ResourceConfig)NetworkScriptableObjectSerializer.DeserializeScriptableObject(resourceName);
+            var resourceData = _resourcesData.FirstOrDefault(x =>
+                x.ResourceConfig.TypeResource == resourceConfig.TypeResource);
+
+            if (resourceData == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Resource data isn't exist");
+#endif
+                return;
+            }
+
+            _resourcesData.Remove(resourceData);
+        }
 
         public override void DropAllResources()
         {
-            foreach (var resourceData in _resourcesData)
+            for (int i = _resourcesData.Count - 1; i >= 0; i--)
             {
-                DropResource(resourceData);
+                DropResource(_resourcesData[i]);
+            }
+        }
+
+        private void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                DropAllResources();
             }
         }
     }
