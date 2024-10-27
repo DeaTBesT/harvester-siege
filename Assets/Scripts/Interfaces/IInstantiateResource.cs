b32e9cbbee6b41a2d3163eda092cbb886cb83a4c
DeaTBesT <niliukhin@gmail.com>
2024-10-28 01:51:29 +0300

    feat: сделал интерфейс создания ресурсов

---
+++ Assets/Scripts/Interfaces/IInstantiateResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces
+{
+    public interface IInstantiateResource
+    {
+        public void InstantiateResource(string resourceName, int amount);
+        public void InstantiateResourceCmd(string resourceName, int amount);
+        public void InstantiateResourceServer(string resourceName, int amount);
+    }
+}
