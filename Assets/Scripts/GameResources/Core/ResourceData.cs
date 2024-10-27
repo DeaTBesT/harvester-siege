6368d0ab21808967de89a8416a145d71230739d6
DeaTBesT <niliukhin@gmail.com>
2024-10-27 03:05:14 +0300

    feat: добавил метод создания ресурса

--- Assets/Scripts/GameResources/Core/ResourceData.cs
+++ Assets/Scripts/GameResources/Core/ResourceData.cs
@@ -51,5 +51,49 @@ namespace GameResources.Core
         [ClientRpc]
         private void RemoveResourceRpc(int amount) =>
             _amount = Mathf.Clamp(_amount - amount, 0, int.MaxValue);
+
+        public static void InstantiateResource(ResourceData resourceData, Vector3 spawnPosition = default,
+            Quaternion spawnRotation = default)
+        {
+            var newResource = Object.Instantiate(resourceData.ResourceConfig.ResourcePrefab, spawnPosition,
+                spawnRotation);
+            NetworkServer.Spawn(newResource);
+
+            if (newResource.TryGetComponent(out GameResource gameResource))
+            {
+                gameResource.SetAmount(resourceData.AmountResource);
+            }
+        }
+
+        public static void InstantiateResource(string resourceName, int amount, Vector3 spawnPosition = default,
+            Quaternion spawnRotation = default)
+        {
+            if (string.IsNullOrEmpty(resourceName))
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Name is null");
+#endif
+                return;
+            }
+            
+            var resourceConfig = (ResourceConfig)NetworkScriptableObjectSerializer.DeserializeScriptableObject(resourceName);
+            
+            if (resourceConfig == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Config isn't exist");
+#endif
+                return;
+            }
+            
+            var newResource = Object.Instantiate(resourceConfig.ResourcePrefab, spawnPosition,
+                spawnRotation);
+            NetworkServer.Spawn(newResource);
+
+            if (newResource.TryGetComponent(out GameResource gameResource))
+            {
+                gameResource.SetAmount(amount);
+            }
+        }
     }
 }
