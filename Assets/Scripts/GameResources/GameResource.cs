db2af343068d2be47790723b0aadb950c922c4fb
DeaTBesT <niliukhin@gmail.com>
2024-10-27 03:09:10 +0300

    feat: сделал загрузчик

--- Assets/Scripts/GameResources/GameResource.cs
+++ Assets/Scripts/GameResources/GameResource.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using GameResources.Core;
+using GameResources.CustomSerialization;
 using Interfaces;
+using Managers;
 using Mirror;
 using Player;
 using UnityEngine;
 
 namespace GameResources
 {
-    public class GameResource : NetworkBehaviour, IInteractable
+    public class GameResource : NetworkBehaviour, IInteractable, INetworkLoad
     {
         [SerializeField] private ResourceData _resourceData;
 
@@ -15,6 +18,33 @@ namespace GameResources
 
         public Action OnInteract { get; set; }
         public Action OnFinishInteract { get; set; }
+
+        public override void OnStartServer() =>
+            NetworkLoadManager.Instance.AddLoader(this);
+
+        public override void OnStopServer() =>
+            NetworkLoadManager.Instance.RemoveLoader(this);
+
+        [Server]
+        public void LoadDataServer(NetworkConnectionToClient conn)
+        {
+            var data = new GameResourceData(_resourceData.AmountResource);
+
+            var writer = new NetworkWriter();
+            writer.WriteGameResourceData(data);
+            var writerData = writer.ToArray();
+
+            LoadDataRpc(conn, writerData);
+        }
+
+        [TargetRpc]
+        public void LoadDataRpc(NetworkConnectionToClient target, byte[] writerData)
+        {
+            var reader = new NetworkReader(writerData);
+            var data = reader.ReadGameResourceData();
+
+            _resourceData.SetAmount(data.Amount);
+        }
 
         public bool TryInteract(IInteractor interactor)
         {
