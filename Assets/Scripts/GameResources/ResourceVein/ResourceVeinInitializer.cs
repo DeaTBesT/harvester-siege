6f8b295414056e575af4d86e049f41918a2dc57e
DeaTBesT <niliukhin@gmail.com>
2024-10-28 01:55:33 +0300

    feat: добавил компонент жилы ресурса

---
+++ Assets/Scripts/GameResources/ResourceVein/ResourceVeinInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Core;
+using GameResources.Core;
+using UnityEngine;
+
+namespace GameResources.ResourceVein
+{
+    public class ResourceVeinInitializer : EntityInitializer
+    {
+        [SerializeField] private ResourceVeinStats _resourceVeinStats;
+        [SerializeField] private ResourceSpawner _resourceSpawner;
+
+        protected override void OnValidate()
+        {
+            if (_resourceVeinStats == null)
+            {
+                _resourceVeinStats = GetComponent<ResourceVeinStats>();
+            }
+            
+            if (_resourceSpawner == null)
+            {
+                _resourceSpawner = GetComponent<ResourceSpawner>();
+            }
+        }
+
+        public override void Initialize() => 
+            _resourceVeinStats.Initialize(_resourceSpawner);
+    }
+}
---
+++ Assets/Scripts/GameResources/ResourceVein/ResourceVeinStats.cs
@@ -0,0 +1,59 @@
+﻿using Core;
+using GameResources.Core;
+using Mirror;
+using UnityEngine;
+
+namespace GameResources.ResourceVein
+{
+    public class ResourceVeinStats : EntityStats
+    {
+        private ResourceSpawner _resourceSpawner;
+        
+        public override int TeamId => 100;
+
+        public override void Initialize(params object[] objects) => 
+            _resourceSpawner = objects[0] as ResourceSpawner;
+
+        public override void TakeDamage(int teamId, float amount)
+        {
+            if (teamId == TeamId)
+            {
+                return;
+            }
+
+            if (_health - amount > 0)
+            {
+                TakeDamageCmd(amount);
+                return;
+            }
+            
+            DestroyEntity();
+        }
+
+        [Command(requiresAuthority = false)]
+        private void TakeDamageCmd(float amount) => 
+            TakeDamageRpc(amount);
+
+        [ClientRpc]
+        private void TakeDamageRpc(float amount) => 
+            _health -= Mathf.Clamp(_health - amount, 0, float.MaxValue);
+
+        public override void DestroyEntity() => 
+            DestroyEntityCmd();
+
+        [Command(requiresAuthority = false)]
+        private void DestroyEntityCmd() => 
+            DestroyEntityRpc();
+
+        [ServerCallback]
+        private void DestroyEntityRpc()
+        {
+            if (_resourceSpawner != null)
+            {
+                _resourceSpawner.InstantiateResource();
+            }
+
+            NetworkServer.Destroy(gameObject);
+        }
+    }
+}
