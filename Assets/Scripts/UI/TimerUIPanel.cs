264ffce1b9068dc10223732fcac045ba93ef0b23
DeaTBesT <niliukhin@gmail.com>
2024-11-14 02:34:21 +0300

    feat: добавил UI панель таймера

---
+++ Assets/Scripts/UI/TimerUIPanel.cs
@@ -0,0 +1,23 @@
+﻿using Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class TimerUIPanel : UIPanel, IInitialize
+    {
+        [SerializeField] private Image _imageTimer;
+
+        private float _durationTime;
+        
+        public bool IsEnable { get; set; }
+        public void Initialize(params object[] objects) => 
+            _durationTime = (float)objects[0];
+
+        public void SetTime(float time)
+        {
+            var amount = time / _durationTime;
+            _imageTimer.fillAmount = amount;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class UIPanel : MonoBehaviour
+    {
+        [SerializeField] private Canvas _panelCanvas;
+        
+        public void Show() => 
+            _panelCanvas.enabled = true;
+
+        public void Hide() => 
+            _panelCanvas.enabled = false;
+    }
+}
