d44a5e8551424d4975fb5442b445f2df8530893e
DeaTBesT <niliukhin@gmail.com>
2024-11-14 02:38:28 +0300

    feat: добавил загрузку изменений

--- Assets/Scripts/GameResources/ResourceVein/ResourceVeinStats.cs
+++ Assets/Scripts/GameResources/ResourceVein/ResourceVeinStats.cs
@@ -1,16 +1,46 @@
 ﻿using Core;
 using GameResources.Core;
+using GameResources.CustomSerialization;
+using Interfaces;
+using Managers;
 using Mirror;
 using UnityEngine;
 
 namespace GameResources.ResourceVein
 {
-    public class ResourceVeinStats : EntityStats
+    public class ResourceVeinStats : EntityStats, INetworkLoad
     {
         private ResourceSpawner _resourceSpawner;
         
         public override int TeamId => 100;
 
+        public override void OnStartServer() => 
+            NetworkLoadManager.Instance.AddLoader(this);
+
+        public override void OnStopServer() => 
+            NetworkLoadManager.Instance.RemoveLoader(this);
+
+        [Server]
+        public void LoadDataServer(NetworkConnectionToClient conn)
+        {
+            var data = new ResourceVeinStatsData(_health);
+
+            var writer = new NetworkWriter();
+            writer.WriteResourceVeinStatsData(data);
+            var writerData = writer.ToArray();
+
+            LoadDataRpc(conn, writerData);
+        }
+
+        [TargetRpc]
+        public void LoadDataRpc(NetworkConnectionToClient target, byte[] writerData)
+        {
+            var reader = new NetworkReader(writerData);
+            var data = reader.ReadResourceVeinStatsData();
+
+            _health = data.Health;
+        }
+        
         public override void Initialize(params object[] objects) => 
             _resourceSpawner = objects[0] as ResourceSpawner;
 
