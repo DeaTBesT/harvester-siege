5ba4a4278e907034d4205cd4e1542ea365d842b9
DeaTBesT <niliukhin@gmail.com>
2024-11-14 02:39:20 +0300

    feat: серилизатор для загрузки данных

---
+++ Assets/Scripts/GameResources/CustomSerialization/ResourceVeinStatsSerializator.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+
+namespace GameResources.CustomSerialization
+{
+    public struct ResourceVeinStatsData
+    {
+        public float Health { get; }
+
+        public ResourceVeinStatsData(float health) => 
+            Health = health;
+    }
+    
+    public static class ResourceVeinStatsSerializator
+    {
+        public static void WriteResourceVeinStatsData(this NetworkWriter writer,
+            ResourceVeinStatsData statsData) =>
+            writer.WriteFloat(statsData.Health);
+
+        public static ResourceVeinStatsData ReadResourceVeinStatsData(this NetworkReader reader) => 
+            new(reader.ReadFloat());
+    }
+}
