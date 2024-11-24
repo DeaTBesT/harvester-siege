3503846561e01978f9cc3a70249965bd3d0d7535
DeaTBesT <niliukhin@gmail.com>
2024-11-24 09:17:40 +0300

    refactor: изменил таймер и его местоположение

---
+++ Assets/Scripts/Utils/TimerSystem/Core/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils.TimerSystem.Core
+{
+    public abstract class Timer
+    {
+        public Action OnTimerStart  { get; set; }
+        public Action<float> OnChangedTime { get; set; }
+        public Action OnTimerForceStop { get; set; } //Принудительное завершение
+        public Action OnTimerStop { get; set; } //Завершение по истечении времени
+        
+        protected MonoBehaviour _behaviour;
+        protected float _timeCountdown;
+ 
+        public Timer(MonoBehaviour behaviour, float time)
+        {
+            _behaviour = behaviour;
+            _timeCountdown = time;
+        }
+
+        public abstract void Start();
+
+        public abstract void Stop();
+    }
+}
